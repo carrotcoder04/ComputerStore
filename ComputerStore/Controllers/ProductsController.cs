782d638800e1ec3d0733f7bfe63374cb80843e6a
Lĩnh MX <135999189+linhplus2022@users.noreply.github.com>
2025-04-23 14:50:00 +0700

    first commit

---
+++ ComputerStore/Controllers/ProductsController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using ComputerStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace ComputerStore.Controllers
+{
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet("alls")]
+        [SwaggerOperation(
+            Summary = "Lấy tất cả sản phẩm",
+            Description = "Trả về danh sách tất cả sản phẩm trong cơ sở dữ liệu."
+        )]
+        public IActionResult GetAll()
+        {
+            List<Product> products = new List<Product>();
+            products.Add(new Product() {
+                Name = "PC Gaming",
+                Price = 12030302,
+                CreateAt = DateTime.Now,
+            });
+            products.Add(new Product() {
+                Name = "PC Văn phòng",
+                Price = 272737823,
+                CreateAt = DateTime.Now,
+            });
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok($"Product {id}");
+        }
+        [HttpPost]
+        public IActionResult Create([FromBody] string name)
+        {
+            return Ok($"Created: {name}");
+        }
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] string name)
+        {
+            return Ok($"Updated {id}: {name}");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok($"Deleted product {id}");
+        }
+    }
+}
---
+++ ComputerStore/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ComputerStore.Models
+{
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public int DiscountPercent { get; set; }
+        public DateTime PromotionEndDate { get; set; }
+        [JsonIgnore]
+        public int CategoryId { get; set; }
+        public int WarrentyPeriod { get; set; }
+        public DateTime CreateAt { get; set; }
+    }
+}
